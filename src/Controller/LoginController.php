6d874e086a55ffeca28a46abe0329de7fb38db7d
zackad <zackad.dev@gmail.com>
2018-11-30 16:06:43 +0700

    [security] implement json authentication

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoginController extends AbstractController
+{
+    /**
+     * @Route("/users/login", name="login")
+     */
+    public function login(Request $request)
+    {
+        $user = $this->getUser();
+        return $this->json([
+            'username' => $user->getUsername(),
+            'roles' => $user->getRoles(),
+        ]);
+    }
+}
