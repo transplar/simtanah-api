2e362b4686337309c30db4a30ebd753aa594833d
zackad <zackad.dev@gmail.com>
2018-12-09 07:05:28 +0700

    [entity] change user entity table name

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
+ * @ORM\Table(name="pengguna")
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
 class User implements UserInterface
