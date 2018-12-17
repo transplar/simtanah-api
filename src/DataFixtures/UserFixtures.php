082c769481c8b5358f7f9c9ab0c906515c111fa4
zackad <zackad.dev@gmail.com>
2018-12-17 08:48:59 +0700

    [fixtures] default user fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\User;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $user = new User;
+        $user->setUsername('admin')
+            ->setPassword($this->passwordEncoder->encodePassword($user, 'admin'))
+        ;
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
