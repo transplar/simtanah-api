15bb563375e6a5d40abaffccb9c691ed33ca7897
zackad <zackad.dev@gmail.com>
2018-12-17 22:17:02 +0700

    [endpoint] update config

--- src/Controller/ConfigController.php
+++ src/Controller/ConfigController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Config;
 use App\Repository\ConfigRepository;
@@ -25,6 +26,30 @@ class ConfigController extends AbstractController
         return $this->json([
             'status' => 'OK',
             'config' => $config,
+        ]);
+    }
+
+    /**
+     * @Route("/config", name="config_update", methods={"PATCH"})
+     */
+    public function update(Request $request, EntityManagerInterface $em, ConfigRepository $configRepository)
+    {
+        if (!$this->getUser()) {
+            return $this->json([
+                'status' => 'ERROR',
+                'message' => 'Anda tidak berwenang mengakses halaman ini.'
+            ], 401);
+        }
+
+        $input = json_decode($request->getContent(), true);
+        $name = $input['name'];
+        $config = $configRepository->findOneBy(['name' => $name]);
+        $config->setContent($input['content']);
+        $em->flush();
+        return $this->json([
+            'status' => 'OK',
+            'message' => 'Config saved successfully',
+            'item' => $config,
         ]);
     }
 }
