36436ecbb3c9451a5383fd0b50df288c6615925c
zackad <zackad.dev@gmail.com>
2018-12-18 11:11:06 +0700

    [endpoint] gallery index

---
+++ src/Controller/GalleryController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\GalleryRepository;
+
+class GalleryController extends AbstractController
+{
+    /**
+     * @Route("/gallery", name="gallery_index", methods={"GET"})
+     */
+    public function index(Request $request, GalleryRepository $galleryRepository)
+    {
+        $limit = $request->query->get('limit') ?? 100;
+        $offset = $request->query->get('page') * $limit ?? 0;
+        $gallery = $galleryRepository->findBy(
+            [],
+            ['eventDate' => 'DESC'],
+            $limit,
+            $offset
+        );
+
+        if (empty($gallery)) {
+            return $this->json([
+                'status' => 'ERROR',
+                'message' => 'Items not found',
+            ], 404);
+        }
+
+        return $this->json([
+            'status' => 'OK',
+            'items' => $gallery,
+        ]);
+    }
+}
