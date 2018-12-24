ef43854084894d28b3b11564b1919adc5010e459
zackad <zackad.dev@gmail.com>
2018-12-24 15:05:44 +0700

    [endpoint] upload file

---
+++ src/Controller/FileUploadController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FileUploadController extends AbstractController
+{
+    /**
+     * @Route("/upload", name="file_upload", methods={"POST"})
+     */
+    public function index(Request $request)
+    {
+        if (!isset($_FILES['file']) || empty($_FILES['file'])) {
+            return $this->json([
+                'status' => 'ERROR',
+                'message' => 'No resource to be uploaded, file cannot be empty.',
+            ], 400);
+        }
+
+        $uploadDir = $this->getParameter('kernel.project_dir');
+        $hashValue = hash_file('crc32', $_FILES['file']['tmp_name']);
+        $filename = $hashValue . '-' . $_FILES['file']['name'];
+        $file = $uploadDir.'/public/upload/'.$filename;
+        move_uploaded_file($_FILES['file']['tmp_name'], $file);
+        $url = $request->getUri(). '/' . $filename;
+
+        return $this->json([
+            'status' => 'OK',
+            'url' => $url,
+            'filename' => $filename,
+        ]);
+    }
+}
